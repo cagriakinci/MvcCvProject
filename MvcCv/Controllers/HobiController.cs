24bbf48f809affa9b18810e9fb21eab399674a3a
Cagri Akinci <cagrikarabas@hotmail.com>
2022-04-29 16:54:32 +0300

    Add project files.

---
+++ MvcCv/Controllers/HobiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+        // GET: Hobi
+        GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hobiler = repo.List();
+            return View(hobiler);
+        }
+        // Index'te zaten listeleme yaptığımız için güncellemenin post tarafınıda Index'ten yaptık.
+        [HttpPost]
+        public ActionResult Index(TblHobilerim p)
+        {
+            var hobiBul = repo.Find(x => x.ID == 1);
+            hobiBul.Aciklama1 = p.Aciklama1;
+            hobiBul.Aciklama2 = p.Aciklama2;
+            repo.TUpdate(hobiBul);
+            return RedirectToAction("Index");
+        }
+    }
+}
